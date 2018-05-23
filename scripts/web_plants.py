0c3f1884a60457271585446971bc4712a88d2e66
Bart Koopman <bkoopman@sdl.com>
2018-05-23 15:47:08 +0200

    fixing favicon

--- scripts/web_plants.py
+++ scripts/web_plants.py
@@ -21,6 +21,10 @@ def hello():
     templateData = template()
     return render_template("main.html", **templateData)
 
+@app.route("/favicon.ico")
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, "static"), "favicon.ico", mimetype="image/x-icon")
+						  
 @app.route("/last_watered")
 def check_last_watered():
     templateData = template(text = water.get_last_watered())
