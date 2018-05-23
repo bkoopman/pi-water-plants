69bbe658e4f82c9cef701e7983b037331af3032d
Bart Koopman <bkoopman@sdl.com>
2018-05-23 15:56:00 +0200

    fixed favicon in script

--- scripts/web_plants.py
+++ scripts/web_plants.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, send_from_directory
 import psutil
 import datetime
 import water
